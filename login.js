35842189173b68938e88323b1262dc3491996c1a
Elias <elias2bernardo2@gmail.com>
2023-04-26 23:12:09 -0500

    cpanel 26 de abril

--- login.js
+++ login.js
@@ -2,9 +2,11 @@ let usuario = document.getElementById('usuario').value;
 
 
 function convertir(){
-    let clave = document.getElementById('clave').value;
-    let clave64 = window.btoa(clave);
-    let decodClave64 = window.atob(clave64);
-    console.log(clave64);
-    console.log(decodClave64);
+  let loader =  document.getElementById("loader");
+  loader.classList.add("loader");
+  let clave = document.getElementById('clave').value;
+  let clave64 = window.btoa(clave);
+  let decodClave64 = window.atob(clave64);
+  console.log(clave64);
+  console.log(decodClave64);
 }
---
+++ readme.txt
@@ -0,0 +1,5 @@
+#paso 1: Instalar Typescript
+-npm install typescript --save-dev
+- npx tsc --version =>para verificar version
+#paso 2 : Crear archivo .gitignore
+#paso 3 : creararchivo .editorconfig
