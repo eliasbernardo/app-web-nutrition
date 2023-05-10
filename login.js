ae9186ae2309be4c2b0b44c6c8f791977c946668
Elias <elias2bernardo2@gmail.com>
2023-05-09 22:10:42 -0500

    modificacion elias 9 mayo

--- login.js
+++ login.js
@@ -1,12 +1,17 @@
-let usuario = document.getElementById('usuario').value;
-
-
-function convertir(){
-  let loader =  document.getElementById("loader");
-  loader.classList.add("loader");
-  let clave = document.getElementById('clave').value;
-  let clave64 = window.btoa(clave);
-  let decodClave64 = window.atob(clave64);
-  console.log(clave64);
-  console.log(decodClave64);
+function login(){
+  let usuario = document.getElementById("usuario").value;
+  let passwrord = document.getElementById("clave").value;
+  if(usuario !='' && passwrord != ''){
+    console.log("no esta vacio");
+    let loader =  document.getElementById("loader");
+    loader.classList.add("loader");
+    let clave = document.getElementById('clave').value;
+    let clave64 = window.btoa(clave);
+    let decodClave64 = window.atob(clave64);
+    console.log(clave64);
+    console.log(decodClave64);
+    avisoMostrar("Validando Datos","success");
+  }else{
+    avisoMostrar("Completa los campos","warning");
+  }
 }
