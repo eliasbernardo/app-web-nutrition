9d7afe021c71816de346c63be43171a913b52ba0
Elias <elias2bernardo2@gmail.com>
2023-05-12 12:24:17 -0500

    prueba comentada

--- code/ts/avisos.ts
+++ code/ts/avisos.ts
@@ -1,5 +1,6 @@
 let container = document.getElementById("container");
 function avisoMostrar(text:string,tipoIcon:string){
+  /*prueba*/
   let bodyAviso = document.createElement("div");
   let mensaje = document.createElement("div");
   let icon = document.createElement("div");
