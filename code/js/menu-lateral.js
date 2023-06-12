7c6c7af9bfe4d86589d7ea0b0060e275d663a811
Elias <elias2bernardo2@gmail.com>
2023-06-11 22:53:50 -0500

    css elias dashboard

--- code/js/menu-lateral.js
+++ code/js/menu-lateral.js
@@ -4,32 +4,29 @@ let loginElements = document.querySelectorAll('.login-usuario');
 
 listElements.forEach(listElements => {
     listElements.addEventListener('click', ()=>{
-        
-        
+
+
         listElements.classList.toggle('arrow');
         let height = 0;
         let menu = listElements.nextElementSibling;
 
         if(menu.clientHeight == "0"){
             height = menu.scrollHeight;
-            
-        }
-        
-        
-        menu.style.height = `${height}px`;
-        
-        
 
+        }
+
+
+        menu.style.height = `${height}px`;
     })
-    
+
 })
 
 loginElements.forEach(loginElements =>{
     loginElements.addEventListener('click', ()=>{
         let height = 0;
-        
+
         let menu = loginElements.nextElementSibling;
-        
+
         if(menu.clientHeight == "0"){
             height = menu.scrollHeight;
         }
